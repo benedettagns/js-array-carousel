6142d6c3dae6aa72c888bc7299e38b8fe2c9ad59
benedettagns <benedetta.genise01@gmail.com>
2021-12-14 17:50:35 +0100

    seconda parte

--- js/script.js
+++ js/script.js
@@ -5,18 +5,45 @@ const titleText = [
     'La terra è rotonda',
 ];
 
-    /*const slideTexts = [
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, dolor dolores error, beatae amet nesciunt distinctio ducimus aut iusto voluptas dicta qui. Vero nulla doloribus nihil assumenda eligendi, dolorum sequi!',
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut harum labore rem ratione omnis asperiores magni ipsa delectus adipisci quam, consectetur fuga dolorem, sed impedit dolores, ad pariatur porro expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolores necessitatibus qui, similique expedita, praesentium, perspiciatis aliquam corrupti aperiam quisquam esse obcaecati voluptatum pariatur eaque dolor nisi ipsa maxime ducimus!',
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis consectetur quidem eius. Reprehenderit doloribus similique doloremque aperiam, nesciunt consectetur sit fugit culpa exercitationem optio quis voluptates unde labore vel aliquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea mollitia est nam impedit provident! Ut obcaecati tempore impedit qui error molestiae a laborum doloribus repellendus, modi est quam nulla iure. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam commodi iste nisi consectetur ratione corrupti doloribus at maxime.',
-    ];*/
+//array testo 
+const slideTexts = [
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, dolor dolores error, beatae amet nesciunt distinctio ducimus aut iusto voluptas dicta qui. Vero nulla doloribus nihil assumenda eligendi, dolorum sequi!',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut harum labore rem ratione omnis asperiores magni ipsa delectus adipisci quam, consectetur fuga dolorem, sed impedit dolores, ad pariatur porro expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolores necessitatibus qui, similique expedita, praesentium, perspiciatis aliquam corrupti aperiam quisquam esse obcaecati voluptatum pariatur eaque dolor nisi ipsa maxime ducimus!',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis consectetur quidem eius. Reprehenderit doloribus similique doloremque aperiam, nesciunt consectetur sit fugit culpa exercitationem optio quis voluptates unde labore vel aliquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea mollitia est nam impedit provident! Ut obcaecati tempore impedit qui error molestiae a laborum doloribus repellendus, modi est quam nulla iure. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam commodi iste nisi consectetur ratione corrupti doloribus at maxime.',
+];
+
 let currentIndex = 0;
 
 const titleSlider = document.querySelector('.slider');
+
+titleSlider.innerHTML = titleText[0];
+
 const nextTitle = document.getElementById('next');
 const prevTitle = document.getElementById('prev');
 
 nextTitle.addEventListener('click', function () {
+    currentIndex++;
+    titleSlider.innerHTML = titleText[currentIndex];
+})
+
+prevTitle.addEventListener('click', function () {
+    currentIndex--;
+    titleSlider.innerHTML = titleText[currentIndex];
+})
+
+const textSlider = document.querySelector('.slider');
+
+textSlider.innerHTML = slideTexts[0];
+
+const nextTexts = document.getElementById('next');
+const prevTexts = document.getElementById('prev');
+
+nextTexts.addEventListener('click', function () {
     currentIndex++;   
-    
+    slideTexts.innerHTML = slideTexts[currentIndex];
+})
+
+prevTexts.addEventListener('click', function () {
+    currentIndex--;
+    slideTexts.innerHTML = slideTexts[currentIndex];
 })
