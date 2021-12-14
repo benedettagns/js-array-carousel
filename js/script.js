3ef0bb79e9bcad9d020107e63047e85c8ae4639a
benedettagns <benedetta.genise01@gmail.com>
2021-12-14 17:20:29 +0100

    prima parte

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//array titolo 
+const titleText = [
+    'Notre-Dame in fiamme',
+    'Uscita nuovo smartphone',
+    'La terra è rotonda',
+];
+
+    /*const slideTexts = [
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, dolor dolores error, beatae amet nesciunt distinctio ducimus aut iusto voluptas dicta qui. Vero nulla doloribus nihil assumenda eligendi, dolorum sequi!',
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut harum labore rem ratione omnis asperiores magni ipsa delectus adipisci quam, consectetur fuga dolorem, sed impedit dolores, ad pariatur porro expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolores necessitatibus qui, similique expedita, praesentium, perspiciatis aliquam corrupti aperiam quisquam esse obcaecati voluptatum pariatur eaque dolor nisi ipsa maxime ducimus!',
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis consectetur quidem eius. Reprehenderit doloribus similique doloremque aperiam, nesciunt consectetur sit fugit culpa exercitationem optio quis voluptates unde labore vel aliquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea mollitia est nam impedit provident! Ut obcaecati tempore impedit qui error molestiae a laborum doloribus repellendus, modi est quam nulla iure. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam commodi iste nisi consectetur ratione corrupti doloribus at maxime.',
+    ];*/
+let currentIndex = 0;
+
+const titleSlider = document.querySelector('.slider');
+const nextTitle = document.getElementById('next');
+const prevTitle = document.getElementById('prev');
+
+nextTitle.addEventListener('click', function () {
+    currentIndex++;   
+    
+})
